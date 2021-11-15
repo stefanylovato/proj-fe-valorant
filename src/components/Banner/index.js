763654df9e15ed02c48eed837c1b1aa3155846ab
Stefany Lovato <stefanylovato@gmail.com>
2021-11-15 00:06:35 -0300

    header n banner

---
+++ src/components/Banner/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+const Banner = () => {
+
+    return(
+        <section id="banner"><iframe width="1520" height="585" src="https://www.youtube.com/embed/Vr8pUinsfn0" title="Valorant funny moments" frameborder="0" allowfullscreen></iframe></section>
+        
+    )
+}
+
+export default Banner;
---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import './style.css';
+
+const Header = () =>{
+
+    return (
+        <header>
+            <h1>Valorant do Amor</h1>
+            <nav>
+                <ul>
+                    <li> <a href="#">Valorant</a></li>
+                    <li> <a href="#galeria">Vídeos</a></li>
+                    <li> <a href="#footer">Footer</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+
+}
+
+export default Header;
